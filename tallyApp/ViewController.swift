977b9215fe29bf8dd0152e38044ca073979e8b2f
Tyler May <tylermay@Tylers-MacBook-Air.local>
2023-09-22 08:27:42 -0600

    Tally app project finished

---
+++ tallyApp/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  tallyApp
+//
+//  Created by Tyler May on 9/21/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var incrimentTextField: UITextField!
+    
+    @IBOutlet weak var resetTextField: UITextField!
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
+
+    var incriment = 1
+    var number = 0
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    @IBAction func increaseButtonTapped(_ sender: Any) {
+
+        if incrimentTextField.text!.isEmpty {
+           incriment = 1
+        } else {
+            incriment = Int(incrimentTextField.text!) ?? 1
+        }
+        number += incriment
+        resultLabel.text = String(number)
+    }
+    
+    @IBAction func resetButtonTapped(_ sender: Any) {
+        resultLabel.text = resetTextField.text
+        number = Int(resetTextField.text!) ?? 1
+    }
+    
+    
+}
+
