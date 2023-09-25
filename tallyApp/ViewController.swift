02fa364e357dfdb912d31a740141d2bd0998f968
Tyler May <tylermay@Tylers-MacBook-Air.local>
2023-09-25 09:27:52 -0600

    tally app black diamond update

--- tallyApp/ViewController.swift
+++ tallyApp/ViewController.swift
@@ -34,9 +34,19 @@ class ViewController: UIViewController {
         resultLabel.text = String(number)
     }
     
+    @IBAction func decreaseButtonTapped(_ sender: Any) {
+        if incrimentTextField.text!.isEmpty {
+            incriment = -1
+        } else {
+            incriment = Int(incrimentTextField.text!) ?? -1
+        }
+        number -= incriment
+        resultLabel.text = String(number)
+    }
+    
     @IBAction func resetButtonTapped(_ sender: Any) {
         resultLabel.text = resetTextField.text
-        number = Int(resetTextField.text!) ?? 1
+        number = Int(resetTextField.text!) ?? 0
     }
     
     
